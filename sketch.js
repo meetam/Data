8e9b1673735f71722764267abf0cfaa6f584a5b7
Meeta Marathe <mbmarath@ucsd.edu>
2019-03-07 22:46:00 -0800

    Collecting data

--- sketch.js
+++ sketch.js
@@ -1,16 +1,20 @@
 var canvas;
-var DP05, DP02;
+var DP05, DP02, DP03;
 var totalPop, agePop, racePop;
+var eduPop, eduAttainment;
+var laborPop;
 
 function preload() {
   DP05 = new Array(8);
   DP02 = new Array(8);
+  DP03 = new Array(8);
 
   // Load csv files
   var index = 10;
   for (var i = 0; i < DP05.length; i++) {
     DP05[i] = loadStrings('DP05/ACS_' + index + '_5YR_DP05_with_ann.csv');
     DP02[i] = loadStrings('DP02/ACS_' + index + '_5YR_DP02_with_ann.csv');
+    DP03[i] = loadStrings('DP03/ACS_' + index + '_5YR_DP03_with_ann.csv');
     index++;
   }
 }
@@ -20,12 +24,15 @@ function setup() {
 
   // Initialize Arrays
   totalPop = new Array(8);
-  agePop = new Array(8)
+  agePop = new Array(8);
+  eduPop = new Array(8);
+  eduAttainment = new Array(8);
 
   // Load info into arrays
   for (var i = 0; i < DP05.length; i++) {
     DP05[i] = (DP05[i][2]).split(',');
     DP02[i] = (DP02[i][2]).split(',');
+    DP03[i] = (DP03[i][2]).split(',');
 
     totalPop[i] = DP05[i][3];
     agePop[i] = new Array(13);
@@ -35,7 +42,17 @@ function setup() {
       i1 += 4;
     }
 
-    console.log(totalPop[i]);
+    eduPop[i] = new Array(6);
+    i1 = 207;
+    for (i2 = 0; i2 < 6; i2++) {
+      eduPop[i][i2] = DP02[i][i1];
+      i1 += 4;
+    }
+    for (i2 = 0; i2 < 8; i2++) {
+      eduAttainment[i][i2] = DP02[i][i1];
+      i1 += 4;
+    }
+
   }
 
 }
